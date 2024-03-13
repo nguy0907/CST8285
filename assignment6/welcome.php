15986ec9049bff71ac368979fd49f8f42033fabf
RichardNWeb <richardnguyenatwork@gmail.com>
2024-03-13 13:50:30 -0400

    adding php

---
+++ assignment6/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<?php  ?>
+
+<h1>If you want a custom greeting message, please do the following: </h1>
+<fieldset>
+<ol>
+<li>In your address bar, append a '?' symbol </li>
+<li>Then write 'firstName=' with your first name next to it</li>
+<li>Then write '&' followed by 'lastName' with your last name next to it</li>
+<li>Hit ENTER and see your customized message in the greeting below</li>
+</ol>
+</fieldset>
+</body>
+</html>
