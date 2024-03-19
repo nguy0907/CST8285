7fde85c2557dfd383ad4320e1d18f4d87683ede3
RichardNWeb <richardnguyenatwork@gmail.com>
2024-03-19 13:54:55 -0400

    a

--- assignment6/currency.php
+++ assignment6/currency.php
@@ -11,19 +11,20 @@
 </head>
 
 <body>
+  <h1>Welcome to the Currency Converter </h1>
   <form action="currency.php" method="post">
 
 
     <fieldset>
-      <label for="convertedFrom">Amount</label>
+      <label>Amount</label>
       <input type="text" id="currencyInput" name="currencyInput">
       <?php $_POST ?>
       <br>
       <label>Convert From: </label>
       <input type="radio" name="convertedFrom" value="CAN">CAN <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
       <input type="radio" name="convertedFrom" value="USD">USD <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-      <input type="radio" name="convertedFrom" value="UER">UER <i class="fa-solid fa-euro-sign"></i>
-      <input type="radio" name="convertedFrom" value="GBP">GBP <i class="fa-solid fa-sterling-sign"></i>
+      <input type="radio" name="convertedFrom" value="EUR">EUR <i class="flag-icon flag-icon-fr flag-icon-squared"></i>
+      <input type="radio" name="convertedFrom" value="GBP">GBP <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
       <input type="radio" name="convertedFrom" value="CHY">CHY <i class="flag-icon flag-icon-cn flag-icon-squared"></i>
     </fieldset>
 
@@ -32,26 +33,105 @@
       <label>Convert To: </label>
       <input type="radio" name="convertedTo" value="CAN">CAN <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
       <input type="radio" name="convertedTo" value="USD">USD <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-      <input type="radio" name="convertedTo" value="UER">UER <i class="fa-solid fa-euro-sign"></i>
-      <input type="radio" name="convertedTo" value="GBP">GBP <i class="fa-solid fa-sterling-sign"></i>
+      <input type="radio" name="convertedTo" value="EUR">EUR <i class="flag-icon flag-icon-fr flag-icon-squared"></i>
+      <input type="radio" name="convertedTo" value="GBP">GBP <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
       <input type="radio" name="convertedTo" value="CHY">CHY <i class="flag-icon flag-icon-cn flag-icon-squared"></i>
     </fieldset>
 
+ 
 
-    <input type="submit" id="conversionConfirmed">
+    <input type="submit" id="conversionConfirmed" value="convert">
     <p>
+
       <?php 
+ $CAN = $_POST['currencyInput']; 
+ $USD = $_POST['currencyInput']; 
+ $EUR = $_POST['currencyInput']; 
+ $GBP = $_POST['currencyInput']; 
+ $CHY = $_POST['currencyInput']; 
 
-if (isset($_POST['currencyInput'])) {
 
-  echo $_POST['currencyInput'];
-  echo $_POST['convertedFrom'];
+if (isset($_POST['currencyInput']) ) {
+
+  // An if statement if you click on a certain currency you want to convert
+  if ($_POST['convertedFrom']== "CAN") {
+    echo '<i class="flag-icon flag-icon-ca flag-icon-squared"></i>';
+   echo $_POST['convertedFrom'];
+    echo $_POST['currencyInput']; 
   echo "=";
-  echo $_POST['convertedTo'];
-}
-?>
 
+
+
+  } else if ($_POST['convertedFrom']== "USD"){
+    echo '<i class="flag-icon flag-icon-us flag-icon-squared"></i>';
+   echo $_POST['convertedFrom'];
+    echo $_POST['currencyInput']; 
+  echo "=";
+
+  } else if ($_POST['convertedFrom']== "EUR"){
+    echo '<i class="flag-icon flag-icon-fr flag-icon-squared"></i>';
+   echo $_POST['convertedFrom'];
+    echo $_POST['currencyInput']; 
+  echo "=";
+ 
+  } else if ($_POST['convertedFrom']== "GBP"){
+    echo '<i class="flag-icon flag-icon-gb flag-icon-squared"></i>';
+   echo $_POST['convertedFrom'];
+    echo $_POST['currencyInput']; 
+  echo "=";
+
+  } else if ($_POST['convertedFrom']== "CHY"){
+    echo '<i class="flag-icon flag-icon-cn flag-icon-squared"></i>';
+   echo $_POST['convertedFrom'];
+    echo $_POST['currencyInput']; 
+  echo "=";
+ 
+  }  else{
+    echo 'Must select a value you want to convert ';
+  }
+
+
+    // An if statement if you click on a certain currency you want to convert into
+    if ($_POST['convertedTo']== "CAN") {
+      echo '<i class="flag-icon flag-icon-ca flag-icon-squared"></i>';
+      echo $_POST['convertedTo'];
+      echo $_POST['currencyInput']; 
+  
+    } else if ($_POST['convertedTo']== "USD"){
+      echo '<i class="flag-icon flag-icon-us flag-icon-squared"></i>';
+      echo $_POST['convertedTo']; 
+        echo 0.74* $_POST['currencyInput'];
+      
+    
+
+    } else if ($_POST['convertedTo']== "EUR"){
+      echo '<i class="flag-icon flag-icon-fr flag-icon-squared"></i>';
+      echo $_POST['convertedTo'];
+      echo 0.92* $_POST['currencyInput'];
+        
+    
+
+    } else if ($_POST['convertedTo']== "GBP"){
+      echo '<i class="flag-icon flag-icon-gb flag-icon-squared"></i>';
+      echo $_POST['convertedTo'];
+      echo $_POST['currencyInput']; 
+      echo 0.79* $_POST['currencyInput'];
+
+
+    } else if ($_POST['convertedTo']== "CHY"){
+      echo '<i class="flag-icon flag-icon-cn flag-icon-squared"></i>';
+      echo $_POST['convertedTo'];
+      echo $_POST['currencyInput']; 
+      echo 7.20 * $_POST['currencyInput'];
+    } else {
+      echo 'Must select a value you want to convert to';
+    }
+
+   
+} 
+?>
     </p>
+  
   </form>
 </body>
 
